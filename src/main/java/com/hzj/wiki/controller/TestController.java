f33a71764e3b83b6ba7aed72a30e5e4b593bbde7
735658168 <735658168@qq.com>
2023-11-30 11:45:57 +0800

    2_7 使用Idea的HttpClient进行测试

--- src/main/java/com/hzj/wiki/controller/TestController.java
+++ src/main/java/com/hzj/wiki/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.hzj.wiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
     //http://127.0.0.1:8080/hello
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
         return "Hello World";
+    }
+
+    @RequestMapping(value = "/hello/post",method = RequestMethod.POST)
+    public  String helloPost(String name){
+        return "Hello World Post,"+name;
     }
 }
