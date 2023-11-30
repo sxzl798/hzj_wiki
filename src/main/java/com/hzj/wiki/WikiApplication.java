b1703731a2fcee80f9a5e1d3a915b54408d9c5cd
735658168 <735658168@qq.com>
2023-11-30 10:02:53 +0800

    2_5 启动日志优化

--- src/main/java/com/hzj/wiki/WikiApplication.java
+++ src/main/java/com/hzj/wiki/WikiApplication.java
@@ -1,12 +1,19 @@
 package com.hzj.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
-
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(WikiApplication.class);
+		Environment environment =application.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址:\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
 		SpringApplication.run(WikiApplication.class, args);
 	}
 
