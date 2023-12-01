50dd95932a39f7d86aa0c9215501c6d4cca24b3a
735658168 <735658168@qq.com>
2023-12-01 09:18:51 +0800

    2_8 增加自定义配置项的示例

--- src/main/java/com/hzj/wiki/controller/TestController.java
+++ src/main/java/com/hzj/wiki/controller/TestController.java
@@ -1,18 +1,20 @@
 package com.hzj.wiki.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class TestController {
+    //测试配置项
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     //http://127.0.0.1:8080/hello
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 
     @RequestMapping(value = "/hello/post",method = RequestMethod.POST)
