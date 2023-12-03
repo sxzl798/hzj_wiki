7d3f5745ad6376b53c2536b3138d2cbf3fce699f
735658168 <735658168@qq.com>
2023-12-03 21:02:58 +0800

    3_4 集成MyBatis，并进行配置

--- doc/all.sql
+++ doc/all.sql
@@ -1,13 +1,8 @@
+DROP TABLE IF EXISTS user;
 CREATE TABLE IF NOT EXISTS `user`(
                                      `id` INT UNSIGNED AUTO_INCREMENT KEY COMMENT '用户编号',
                                      `username` VARCHAR(20) NOT NULL UNIQUE COMMENT '用户名',
-                                     `password` CHAR(32) NOT NULL COMMENT '密码',
-                                     `email` VARCHAR(50) NOT NULL UNIQUE COMMENT '邮箱',
-                                     `age` TINYINT UNSIGNED NOT NULL DEFAULT 18 COMMENT '年龄',
-                                     `sex` ENUM('man','woman','baomi') NOT NULL DEFAULT 'baomi' COMMENT '性别',
-                                     `tel` CHAR(11) NOT NULL UNIQUE COMMENT '电话',
-                                     `addr` VARCHAR(50) NOT NULL DEFAULT 'beijing' COMMENT '地址',
-                                     `card` CHAR(18) NOT NULL UNIQUE COMMENT '身份证号',
-                                     `married` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '0代表未结婚，1代表已结婚',
-                                     `salary` FLOAT(8,2) NOT NULL DEFAULT 0 COMMENT '薪水'
+                                     `password` VARCHAR(20) NOT NULL COMMENT '密码'
 )ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COMMENT ='测试';
+
+SELECT 'id','name','password' FROM user;
