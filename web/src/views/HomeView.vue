2d75de92c0dd882736b4b2145985398146b04ef3
735658168 <735658168@qq.com>
2023-12-06 19:25:19 +0800

    5-4 加入Ant Design Vue list组件

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -46,10 +46,38 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ ebooks }}
-{{ ebooks2 }}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
@@ -57,7 +85,34 @@
 <script lang="ts">
 import { defineComponent ,onMounted ,ref ,reactive ,toRef} from 'vue';
 import {LaptopOutlined, NotificationOutlined, UserOutlined} from "@ant-design/icons-vue"; // @ is an alias to /src
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from "axios";
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
+const pagination = {
+  onChange: (page: number) => {
+    console.log(page);
+  },
+  pageSize: 3,
+};
+const actions: Record<string, any>[] = [
+  { icon: StarOutlined, text: '156' },
+  { icon: LikeOutlined, text: '156' },
+  { icon: MessageOutlined, text: '2' },
+];
 export default defineComponent({
   name: 'HomeView',
   setup(){
@@ -76,12 +131,21 @@ export default defineComponent({
     });
     return {
       ebooks,
-      ebooks2: toRef(ebooks1,"books")
-
+      ebooks2: toRef(ebooks1,"books"),
+      listData,
+      pagination:{
+        onChange: (page:any) => {
+          console.log(page);
+        },
+        pageSize:3,
+      },
+      actions
     }
   },
+
   components: {
     NotificationOutlined, LaptopOutlined, UserOutlined,
+    StarOutlined, LikeOutlined, MessageOutlined
   },
 });
 </script>
