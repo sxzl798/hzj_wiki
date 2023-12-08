10d05d46586679972c56e4511967fd79bc3c2062
735658168 <735658168@qq.com>
2023-12-08 16:04:20 +0800

    6-3 修改电子书管理页面布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div class="AdminEbook">
-    <h1>AdminEbook</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="AdminEbook">
+        <h1>AdminEbook</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
+
 </template>
-
-<style scoped>
-
-</style>
+<script setup lang="ts">
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from "@ant-design/icons-vue";
+</script>
