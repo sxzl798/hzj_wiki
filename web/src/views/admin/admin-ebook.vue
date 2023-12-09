5e28dbce69f0c8ec0e3346684d367d1bb18b10ea
735658168 <735658168@qq.com>
2023-12-09 11:23:25 +0800

    6-6 电子书管理页面前后端分页整合成功

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -41,7 +41,7 @@
       const ebooks = ref();
       const pagination = ref({
         current: 1,
-        pageSize: 3,
+        pageSize: 4,
         total: 0
       });
       const loading = ref(false);
@@ -87,12 +87,18 @@
       //数据查询
       const handleQuery = (params: any) => {
         loading.value = true;
-        axios.get("/ebook/list", params).then((response) => {
+        axios.get("/ebook/list", {
+          params:{
+            page: params.page,
+            size: params.size
+          }
+        }).then((response) => {
           loading.value = false;
           const data = response.data;
-          ebooks.value = data.content;
+          ebooks.value = data.content.list;
           //重置分页按钮
           pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
         });
       };
 
@@ -106,7 +112,10 @@
       };
 
       onMounted(() => {
-        handleQuery({});
+        handleQuery({
+          page: 1,
+          size: pagination.value.pageSize
+        });
       });
       return {
         ebooks,
