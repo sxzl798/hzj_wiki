c702301cc09e403e53ba1dac2238d4819024ec3a
735658168 <735658168@qq.com>
2023-12-11 19:56:49 +0800

    6-8 完成电子书编辑功能，编辑成功后刷新列表查询接口参数改为EbookQueryReq，EbookQueryResp

---
+++ src/main/java/com/hzj/wiki/req/EbookQueryReq.java
@@ -0,0 +1,26 @@
+package com.hzj.wiki.req;
+
+public class EbookQueryReq extends PageReq{
+    private Long id;
+
+    private String name = null;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
