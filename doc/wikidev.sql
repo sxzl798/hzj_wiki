e24823b9c0eeef4efdbc3b4509fe244e5dcce6b0
735658168 <735658168@qq.com>
2023-12-12 15:24:18 +0800

    7-2 分类表设计与持久层代码生成，基本的无限级树型表结构设计：id+parent

--- doc/wikidev.sql
+++ doc/wikidev.sql
@@ -43,3 +43,44 @@ INSERT INTO `ebook`(id,name,description) VALUES(2,'Vue入门教程','零基础�
 INSERT INTO `ebook`(id,name,description) VALUES(3,'Python 入门教程','零基础入门Python开发，企业级应用开发最佳首选框架');
 INSERT INTO `ebook`(id,name,description) VALUES(4,'Mysql入门教程','零基础入门Mysql开发，企业级应用开发最佳首选框架');
 INSERT INTO `ebook`(id,name,description) VALUES(5,'Oracle入门教程','零基础入门Oracle开发，企业级应用开发最佳首选框架');
+
+# 分类
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`(
+    `id` BIGINT NOT NULL COMMENT 'id',
+    `parent` BIGINT NOT NULL DEFAULT 0 COMMENT '父id',
+    `name` VARCHAR(50) NOT NULL COMMENT '名称',
+    `sort` INT COMMENT '顺序',
+    PRIMARY KEY (`id`)
+) ENGINE = innodb DEFAULT CHARSET = utf8mb4 COMMENT '分类';
+
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (100,000,'前端开发',100);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (101,100,'Vue',101);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (102,100,'HTML & CSS',102);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (200,000,'Java',200);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (201,200,'基础应用',201);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (202,200,'框架应用',202);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (300,000,'Python',300);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (301,300,'基础应用',301);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (302,300,'进阶方向应用',302);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (400,000,'数据库',400);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (401,400,'MySql',401);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (500,500,'其他',500);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (501,500,'服务器',502);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (502,500,'开发工具',502);
+INSERT INTO `category` (ID, PARENT, NAME, SORT)
+VALUES (503,500,'热门服务端语言',503);
