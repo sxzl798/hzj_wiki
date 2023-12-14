15bab3a46bfbee3b1b9411448ae2cc6a855a20da
735658168 <735658168@qq.com>
2023-12-14 23:46:50 +0800

    8-8 文档内容表设计与代码生成

--- doc/wikidev.sql
+++ doc/wikidev.sql
@@ -110,3 +110,11 @@ INSERT INTO `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
 VALUES (5, 1, 3,'文档2.2', 2, 0, 0);
 INSERT INTO `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
 VALUES (6, 1, 5,'文档2.2.1', 1, 0, 0);
+
+DROP TABLE IF EXISTS `content`;
+CREATE TABLE `content` (
+    `id` BIGINT NOT NULL COMMENT '文档id',
+    `content` MEDIUMTEXT NOT NULL COMMENT '内容',
+    PRIMARY KEY (`id`)
+)ENGINE = innodb DEFAULT CHARSET = utf8mb4 COMMENT ='文档内容';
+
---
+++ src/main/java/com/hzj/wiki/mapper/ContentMapper.java
@@ -0,0 +1,34 @@
+package com.hzj.wiki.mapper;
+
+import com.hzj.wiki.domain.Content;
+import com.hzj.wiki.domain.ContentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ContentMapper {
+    long countByExample(ContentExample example);
+
+    int deleteByExample(ContentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Content row);
+
+    int insertSelective(Content row);
+
+    List<Content> selectByExampleWithBLOBs(ContentExample example);
+
+    List<Content> selectByExample(ContentExample example);
+
+    Content selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") Content row, @Param("example") ContentExample example);
+
+    int updateByExampleWithBLOBs(@Param("row") Content row, @Param("example") ContentExample example);
+
+    int updateByExample(@Param("row") Content row, @Param("example") ContentExample example);
+
+    int updateByPrimaryKeySelective(Content row);
+
+    int updateByPrimaryKeyWithBLOBs(Content row);
+}
