2bbf0a2450dc02d4009f6aace6ceaf869c7d5ff8
735658168 <735658168@qq.com>
2023-12-18 19:02:47 +0800

    9-8 前端增加登录框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,35 +1,73 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <a-row>
+      <a-col :span="23">
     <a-menu
         theme="dark"
         mode="horizontal"
-        :style="{ lineHeight: '64px' }"
+        :style="{ lineHeight: '64px'}"
     >
-      <a-menu-item key="/">
-        <router-link to="/">首页</router-link>
-      </a-menu-item>
-      <a-menu-item key="/admin/user">
-        <router-link to="/admin/user">用户管理</router-link>
-      </a-menu-item>
-      <a-menu-item key="/admin/ebook">
-        <router-link to="/admin/ebook">电子书管理</router-link>
-      </a-menu-item>
-      <a-menu-item key="/admin/category">
-        <router-link to="/admin/category">分类管理</router-link>
-      </a-menu-item>
-      <a-menu-item key="/about">
-        <router-link to="/about">关于我们</router-link>
-      </a-menu-item>
+          <a-menu-item key="/">
+            <router-link to="/">首页</router-link>
+          </a-menu-item>
+          <a-menu-item key="/admin/user">
+            <router-link to="/admin/user">用户管理</router-link>
+          </a-menu-item>
+          <a-menu-item key="/admin/ebook">
+            <router-link to="/admin/ebook">电子书管理</router-link>
+          </a-menu-item>
+          <a-menu-item key="/admin/category">
+            <router-link to="/admin/category">分类管理</router-link>
+          </a-menu-item>
+          <a-menu-item key="/about">
+            <router-link to="/about">关于我们</router-link>
+          </a-menu-item>
     </a-menu>
+      </a-col>
+    <a-col :span="1">
+      <a>
+        <a-avatar style="color: #f56a00; background-color: #fde3cf" @click="showLoginModal">U</a-avatar>
+      </a>
+    </a-col>
+    </a-row>
   </a-layout-header>
+
+  <a-modal v-model:open="loginOpen" title="用户登录"
+           :confirm-loading="loginConfirmLoading"
+           @ok="loginHandleOk">
+
+    <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{span: 16}">
+
+      <a-form-item label="用户名">
+        <a-input v-model:value="loginUser.loginName"/>
+      </a-form-item>
+      <a-form-item label="密码">
+        <a-input-password v-model:value="loginUser.password" type="password" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import {ref} from "vue";
 
-export default defineComponent({
-  name: 'the-header',
-
+const loginUser = ref({
+  loginName: "test",
+  password: "test"
 });
+const loginOpen = ref<boolean>(false);
+const loginConfirmLoading = ref<boolean>(false);
+const showLoginModal = () =>{
+  loginOpen.value = true;
+}
+
+const loginHandleOk = () => {
+  console.log("开始登录");
+};
 </script>
+
+<style>
+.login-menu{
+  float: right !important;
+}
+</style>
