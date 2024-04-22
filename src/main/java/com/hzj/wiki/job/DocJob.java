05f42a7ff4c5542f2b67669a5373048fae7ce9f8
sxzl798 <sxzl798@gmail.com>
2024-04-22 09:47:34 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/hzj/wiki/job/DocJob.java
+++ src/main/java/com/hzj/wiki/job/DocJob.java
@@ -1,9 +1,11 @@
 package com.hzj.wiki.job;
 
 import com.hzj.wiki.service.DocService;
+import com.hzj.wiki.util.SnowFlake;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,8 @@ public class DocJob {
     private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
     @Resource
     private DocService docService;
+    @Resource
+    private SnowFlake snowFlake;
     /**
      * 固定实践间隔，fixedRate单位毫秒
      */
@@ -30,6 +34,8 @@ public class DocJob {
 
     @Scheduled(cron = "5/30 * * * * ? ")
     public void cron() {
+        //设置日志ID，用于追踪日志
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档数据开始");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
