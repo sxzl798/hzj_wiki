2678d2739e555c5474e4c0f988e893b47e7b0226
sxzl798 <sxzl798@gmail.com>
2024-05-10 17:26:32 +0800

    Reapply "10-8 集成WebSocket"
    This reverts commit 293113d5df77ecc2e9a955935df6c81672008800.

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -8,12 +8,58 @@
 </template>
 
 <script lang="ts">
-import {defineComponent,computed} from "vue";
+import {defineComponent,computed,onMounted} from "vue";
 import store from "@/store";
+import {Tool} from "@/util/tool";
 export default defineComponent({
   name:'the-footer',
   setup(){
     const user = computed(()=> store.state.user);
+
+    let websocket:any;
+    let token:any;
+    const onOpen = () => {
+      console.log('websocket连接成功，状态码：', websocket.readyState);
+    };
+
+    const onMessage = (event:any) => {
+      console.log('websocket接收到消息：', event.data);
+    };
+
+    const onError = () => {
+      console.log('websocket连接出错,状态码：',websocket.readyState);
+    };
+
+    const onClose = () => {
+      console.log('websocket连接关闭,状态码：',websocket.readyState);
+    };
+
+    const initWebSocket = () => {
+      //连接成功
+      websocket.onopen = onOpen;
+      //接收到消息
+      websocket.onmessage = onMessage;
+      //连接出错
+      websocket.onerror = onError;
+      //连接关闭
+      websocket.onclose = onClose;
+    };
+
+    onMounted(() => {
+      //WebSocket连接
+      if ('WebSocket' in window){
+        //获取token
+        token = Tool.uuid(10);
+        //连接地址：ws://localhost:8080/ws/xxx
+        websocket = new WebSocket(process.env.VUE_APP_WS_SERVER + '/ws/'+token);
+        initWebSocket();
+        //关闭
+        //websocket.close();
+      }else {
+        alert('当前浏览器不支持WebSocket')
+      }
+    });
+
     return{
       user
     }
