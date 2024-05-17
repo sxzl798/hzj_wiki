20ece207102955075512cc3dab79aba3eb720a17
sxzl798 <sxzl798@gmail.com>
2024-05-17 17:30:14 +0800

    11-7 集成echarts5.5.0 增加示例

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -84,6 +84,12 @@
         </a-card>
       </a-col>
     </a-row>
+    <br>
+    <a-row>
+      <a-col :span="24">
+        <div id="main" style="width: 100%; height: 300px;"></div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
@@ -96,6 +102,9 @@ const statistic = ref({
   viewCount: 0,
   // 其他属性的默认值
 });
+
+declare let echarts: any;
+
 
 const getStatistic = () => {
   axios.get('/ebook-snapshot/get-statistic').then((response) => {
@@ -123,10 +132,37 @@ const getStatistic = () => {
 
     }
   });
+};
+
+// 图表
+const testEcharts = () => {
+// 基于准备好的dom，初始化echarts实例
+  const myChart = echarts.init(document.getElementById('main'));
+// 绘制图表
+  myChart.setOption({
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    xAxis: {
+      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  });
 }
+
+
 
 onMounted(() => {
   getStatistic();
+  testEcharts();
 });
 
 </script>
